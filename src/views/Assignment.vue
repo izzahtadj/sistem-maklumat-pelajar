a0b5332a383f5a24e9da9576dc13055ae0fe70f0
izzahtadj <92962830+izzahtadj@users.noreply.github.com>
2021-10-22 10:33:48 +0800

    Update Assignment.vue

--- src/views/Assignment.vue
+++ src/views/Assignment.vue
@@ -71,21 +71,6 @@
       return{
         newTaskTitle: '',
         tasks: [
-         // {
-         //   id: 1,
-         //   title: 'Wake up',
-         //   done: false            
-         // },
-         // {
-         //   id: 2,
-         //   title: 'Get bananas',
-         //   done: false
-         // },
-         // {
-         //   id: 3,
-         //   title: 'Eat bananas',
-         //   done: false
-         // }
         ]
       }
     },
